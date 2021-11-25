809477e41b8df28b7076b314722b77e9a525157f
Oscar Andrés Úsuga Nanclares <oscarandresu@gmail.com>
2021-11-24 22:21:52 -0500

    Adding Mamifero implementation

--- src/main/java/zooAnimales/Mamifero.java
+++ src/main/java/zooAnimales/Mamifero.java
@@ -1,4 +1,35 @@
 package zooAnimales;
 
-public class Mamifero {
+import gestion.Zona;
+
+import java.util.List;
+
+public class Mamifero extends Animal {
+
+    public static int caballos;
+    public static int leones;
+
+    private List<Mamifero> listado;
+    private boolean pelaje;
+    private int patas;
+
+    public Mamifero(String nombre, int edad, String habitat, String genero, Zona zona, boolean pelaje, int patas) {
+        super(nombre, edad, habitat, genero, zona);
+        this.pelaje = pelaje;
+        this.patas = patas;
+    }
+
+    public Mamifero() { }
+
+    public int cantidadMamiferos() {
+        return 0;
+    }
+
+    public void crearCaballo(Mamifero caballo) {
+        caballos++;
+    }
+
+    public void crearLeon(Mamifero leon) {
+        leones++;
+    }
 }
