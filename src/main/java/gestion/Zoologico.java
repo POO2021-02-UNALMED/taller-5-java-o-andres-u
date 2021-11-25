5e9942eaf71f47c203ff7f7badd7c419e44ed8a8
Oscar Andrés Úsuga Nanclares <oscarandresu@gmail.com>
2021-11-24 21:39:11 -0500

    Adding Zoologico basic implementation

--- src/main/java/gestion/Zoologico.java
+++ src/main/java/gestion/Zoologico.java
@@ -1,4 +1,24 @@
 package gestion;
 
 public class Zoologico {
+
+    private String nombre;
+    private String ubicacion;
+    private Zona[] zonas;
+
+    public Zoologico(String nombre, String ubicacion, Zona[] zonas) {
+        this.nombre = nombre;
+        this.ubicacion = ubicacion;
+        this.zonas = zonas;
+    }
+
+    public Zoologico() {}
+
+    public void agregarZonas(Zona zona) {
+
+    }
+
+    public int cantidadTotalAnimales() {
+        return 0;
+    }
 }
