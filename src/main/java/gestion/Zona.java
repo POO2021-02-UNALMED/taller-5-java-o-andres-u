4f8e2372456dbd4c2e5b7b52113839559cd9b037
Oscar Andrés Úsuga Nanclares <oscarandresu@gmail.com>
2021-11-24 21:54:04 -0500

    Adding Zona implementation and refactoring Zoologico

--- src/main/java/gestion/Zona.java
+++ src/main/java/gestion/Zona.java
@@ -1,4 +1,33 @@
 package gestion;
 
+import zooAnimales.Animal;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Zona {
+
+    private String nombre;
+    private Zoologico zoo;
+    private List<Animal> animales;
+
+    public Zona(String nombre, Zoologico zoo) {
+        this.nombre = nombre;
+        this.zoo = zoo;
+        this.animales = new ArrayList<>();
+    }
+
+    public Zona() {}
+
+    public void agregarAnimales(Animal animal) {
+        animales.add(animal);
+    }
+
+    public int cantidadAnimales() {
+        return animales.size();
+    }
+
+    public List<Animal> getAnimales() {
+        return animales;
+    }
 }
--- src/main/java/gestion/Zoologico.java
+++ src/main/java/gestion/Zoologico.java
@@ -1,24 +1,29 @@
 package gestion;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Zoologico {
 
     private String nombre;
     private String ubicacion;
-    private Zona[] zonas;
+    private List<Zona> zonas;
 
-    public Zoologico(String nombre, String ubicacion, Zona[] zonas) {
+    public Zoologico(String nombre, String ubicacion) {
         this.nombre = nombre;
         this.ubicacion = ubicacion;
-        this.zonas = zonas;
+        this.zonas = new ArrayList<>();
     }
 
     public Zoologico() {}
 
     public void agregarZonas(Zona zona) {
-
+        zonas.add(zona);
     }
 
     public int cantidadTotalAnimales() {
-        return 0;
+        return zonas.stream()
+                .mapToInt(zona -> zona.getAnimales().size())
+                .sum();
     }
 }
