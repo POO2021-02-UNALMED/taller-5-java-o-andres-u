dbaf6de94942528055888fbad03ff58e9be84aca
Oscar Andrés Úsuga Nanclares <oscarandresu@gmail.com>
2021-11-26 00:17:44 -0500

    Adding Reptil implementation

--- src/main/java/zooAnimales/Reptil.java
+++ src/main/java/zooAnimales/Reptil.java
@@ -1,4 +1,38 @@
 package zooAnimales;
 
-public class Reptil {
+import java.util.List;
+
+public class Reptil extends Animal {
+
+    public static int iguanas;
+    public static int serpientes;
+
+    private List<Reptil> listado;
+    private String colorEscamas;
+    private int largoCola;
+
+    public Reptil(String nombre, int edad, String habitat, String genero, String colorEscamas, int largoCola) {
+        super(nombre, edad, habitat, genero);
+        this.colorEscamas = colorEscamas;
+        this.largoCola = largoCola;
+    }
+
+    public Reptil() { }
+
+    public int cantidadReptiles() {
+        return 0;
+    }
+
+    @Override
+    public String movimiento() {
+        return "none";
+    }
+
+    public void crearIguana() {
+
+    }
+
+    public void crearSerpiente() {
+        
+    }
 }
