1d7e1fee549acdf99cbf3c3ca527d210f3be1d69
Oscar Andrés Úsuga Nanclares <oscarandresu@gmail.com>
2021-11-26 00:26:51 -0500

    Adding Anfibio implementation

--- src/main/java/zooAnimales/Anfibio.java
+++ src/main/java/zooAnimales/Anfibio.java
@@ -1,4 +1,34 @@
 package zooAnimales;
 
-public class Anfibio {
+import java.util.List;
+
+public class Anfibio extends Animal {
+
+    public static int ranas;
+    public static int salamandras;
+
+    private List<Anfibio> listado;
+    private String colorPiel;
+    private boolean venenoso;
+
+    public Anfibio(String nombre, int edad, String habitat, String genero, String colorPiel, boolean venenoso) {
+        super(nombre, edad, habitat, genero);
+        this.colorPiel = colorPiel;
+        this.venenoso = venenoso;
+    }
+
+    public Anfibio() { }
+
+    public int cantidadAnfibios() {
+        return 0;
+    }
+
+    @Override
+    public String movimiento() {
+        return "none";
+    }
+
+    public void crearRana() { }
+
+    public void crearSalamandra() { }
 }
